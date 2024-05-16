4887db4569444594271b5adc1eddb49fd68a69a2
Bruno Boucard <boucard.bruno@free.fr>
2024-05-16 15:26:23 +0200

    Rename a few names

--- SeatingPlaceSuggestions/CSharp/TheaterSuggestions.Tests/IntergrationTests/ExternalDependenciesShould.cs
+++ SeatingPlaceSuggestions/CSharp/TheaterSuggestions.Tests/IntergrationTests/ExternalDependenciesShould.cs
@@ -15,7 +15,7 @@ public class ExternalDependenciesShould
     public void Allow_us_to_retrieve_AuditoriumLayout_for_a_given_ShowId()
     {
         var auditoriumLayoutRepository = new AuditoriumLayoutRepository();
-        var auditoriumDto = auditoriumLayoutRepository.GetAuditoriumSeatingFor("2");
+        var auditoriumDto = auditoriumLayoutRepository.FindByShowId("2");
 
         Check.That(auditoriumDto.Rows).HasSize(6);
         Check.That(auditoriumDto.Corridors).HasSize(2);
