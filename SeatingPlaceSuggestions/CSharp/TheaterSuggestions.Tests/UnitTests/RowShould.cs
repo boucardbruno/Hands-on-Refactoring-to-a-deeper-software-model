9182bbe6035698a2866cd042da7ee58edf505d50
Bruno Boucard <boucard.bruno@free.fr>
2024-05-16 19:34:43 +0200

    Add prototype for rowShould

--- SeatingPlaceSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/RowShould.cs
+++ SeatingPlaceSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/RowShould.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NFluent;
 using NUnit.Framework;
 
@@ -24,5 +25,37 @@ public class RowShould
 
         Check.That(rowSecondInstance).IsEqualTo(rowFirstInstance);
         Check.That(newRowWithNewSeatAdded).IsNotEqualTo(rowFirstInstance);
+    }
+
+    [Test]
+    public void
+        Offer_seating_places_from_the_middle_of_the_row_when_the_row_size_is_even_and_party_size_is_greater_than_one()
+    {
+        var partySize = 2;
+
+        var a1 = new SeatingPlace("A", 1, PricingCategory.Second, SeatingPlaceAvailability.Available);
+        var a2 = new SeatingPlace("A", 2, PricingCategory.Second, SeatingPlaceAvailability.Available);
+        var a3 = new SeatingPlace("A", 3, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a4 = new SeatingPlace("A", 4, PricingCategory.First, SeatingPlaceAvailability.Reserved);
+        var a5 = new SeatingPlace("A", 5, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a6 = new SeatingPlace("A", 6, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a7 = new SeatingPlace("A", 7, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a8 = new SeatingPlace("A", 8, PricingCategory.First, SeatingPlaceAvailability.Reserved);
+        var a9 = new SeatingPlace("A", 9, PricingCategory.Second, SeatingPlaceAvailability.Available);
+        var a10 = new SeatingPlace("A", 10, PricingCategory.Second, SeatingPlaceAvailability.Available);
+
+        var row = new Row("A", new List<SeatingPlace> { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 });
+
+        var seatingPlaces = OfferSeatsNearerTheMiddleOfTheRow(row, PricingCategory.Mixed)
+            .Take(partySize);
+
+        Check.That(seatingPlaces)
+            .ContainsExactly(a5, a6);
+    }
+
+    // Deep Modeling: probing the code should start with a prototype.
+    private IEnumerable<SeatingPlace> OfferSeatsNearerTheMiddleOfTheRow(Row row, PricingCategory pricingCategory)
+    {
+        return new List<SeatingPlace>();
     }
 }
