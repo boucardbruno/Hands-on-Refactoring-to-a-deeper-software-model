79b743cec260ed404833f9bd207b4c76bda04aa0
Kenny Baas-Schwegler <kenny@baasie.com>
2024-05-16 18:44:00 +0200

    rename test class

--- SeatingPlaceSuggestions/Java/SeatingPlaceSuggestionsAcceptanceTests/src/test/java/org/weaveit/seatssuggestionsacceptancetests/SeatingPlaceRecommenderTest.java
+++ SeatingPlaceSuggestions/Java/SeatingPlaceSuggestionsAcceptanceTests/src/test/java/org/weaveit/seatssuggestionsacceptancetests/SeatingPlaceRecommenderTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class SeatingPlaceSuggestingMakerTest {
+class SeatingPlaceRecommenderTest {
 
 
     @Test
