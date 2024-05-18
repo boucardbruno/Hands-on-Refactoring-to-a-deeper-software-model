a2f22f2d1ffb378308d5fd34cfadce7afc59874c
Bruno Boucard <boucard.bruno@free.fr>
2024-05-18 07:46:52 +0200

    Withdrawal from seat acceptance tests (3 instead of 4). Use of Disable annotation to ignore the lab2 test

--- SeatingPlaceSuggestions/Java/SeatingPlaceSuggestions/src/test/java/org/weaveit/seatssuggestions/RowTest.java
+++ SeatingPlaceSuggestions/Java/SeatingPlaceSuggestions/src/test/java/org/weaveit/seatssuggestions/RowTest.java
@@ -1,5 +1,6 @@
 package org.weaveit.seatssuggestions;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,35 +27,35 @@ class RowTest {
         assertEquals(rowFirstInstance, rowSecondInstance, "Instance should not mutate existing instance");
     }
 
-//    *** Test for Second Lab ***
-//    @Test
-//    void suggestPlacesOptionsCloserToTheCenterOfARowWhenXHappened() {
-//        int partySize = 2;
-//
-//        SeatingPlace a1 = new SeatingPlace("A", 1, PricingCategory.SECOND, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a2 = new SeatingPlace("A", 2, PricingCategory.SECOND, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a3 = new SeatingPlace("A", 3, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a4 = new SeatingPlace("A", 4, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a5 = new SeatingPlace("A", 5, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a6 = new SeatingPlace("A", 6, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a7 = new SeatingPlace("A", 7, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a8 = new SeatingPlace("A", 8, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a9 = new SeatingPlace("A", 9, PricingCategory.SECOND, SeatingPlaceAvailability.AVAILABLE);
-//        SeatingPlace a10 = new SeatingPlace("A", 10, PricingCategory.SECOND, SeatingPlaceAvailability.AVAILABLE);
-//
-//        Row row = new Row("A", Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10));
-//
-//        List<SeatingPlace> seatingPlaces = suggestPlacesOptionsCloserToTheCenterOfARow(row, PricingCategory.MIXED)
-//                .stream()
-//                .limit(partySize)
-//                .collect(Collectors.toList());
-//
-//        assertEquals(Arrays.asList(a5, a6), seatingPlaces);
-//    }
-//
-//    private List<SeatingPlace> suggestPlacesOptionsCloserToTheCenterOfARow(Row row, PricingCategory pricingCategory) {
-//        // Implement your method logic here
-//        return new ArrayList<>();
-//    }
+    @Test
+    @Disabled("Test for Second Lab")
+    void suggestPlacesOptionsCloserToTheCenterOfARowWhenXHappened() {
+        int partySize = 2;
 
+        SeatingPlace a1 = new SeatingPlace("A", 1, PricingCategory.SECOND, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a2 = new SeatingPlace("A", 2, PricingCategory.SECOND, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a3 = new SeatingPlace("A", 3, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a4 = new SeatingPlace("A", 4, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a5 = new SeatingPlace("A", 5, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a6 = new SeatingPlace("A", 6, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a7 = new SeatingPlace("A", 7, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a8 = new SeatingPlace("A", 8, PricingCategory.FIRST, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a9 = new SeatingPlace("A", 9, PricingCategory.SECOND, SeatingPlaceAvailability.AVAILABLE);
+        SeatingPlace a10 = new SeatingPlace("A", 10, PricingCategory.SECOND, SeatingPlaceAvailability.AVAILABLE);
+
+        Row row = new Row("A", Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10));
+
+        List<SeatingPlace> seatingPlaces = suggestPlacesOptionsCloserToTheCenterOfARow(row, PricingCategory.MIXED)
+                .stream()
+                .limit(partySize)
+                .collect(Collectors.toList());
+
+        assertEquals(Arrays.asList(a5, a6), seatingPlaces);
+    }
+
+    // Deep Modeling: probing the code should start with a prototype
+    private List<SeatingPlace> suggestPlacesOptionsCloserToTheCenterOfARow(Row row, PricingCategory pricingCategory) {
+        // Implement your method logic here
+        return new ArrayList<>();
+    }
 }
